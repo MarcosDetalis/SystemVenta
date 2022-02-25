06eeb4903d411fa56b55d473970bbcbe98329a97
MarcosDetalis <95556582+MarcosDetalis@users.noreply.github.com>
2022-02-25 15:06:33 -0300

    Update Config.php

--- SistemaVenta/Config/Config.php
+++ SistemaVenta/Config/Config.php
@@ -3,7 +3,7 @@ const base_url ="http://localhost:40400/CrearSystem/";
 const host = "localhost";
 const user = "root";
 const pass = "";
-const db = "dbeprueba";
+const db = "dbSport";
 const charset = "charset=utf8";
 ?>
  
